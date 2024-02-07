c8dad365312601716d1fa8ec6b4a94351e077d44
Jaka <jakaambrus1999@gmail.com>
2024-02-07 22:10:19 +0200

    Initial api structure

---
+++ src/MetaExchange.API/Controllers/TradeController.cs
@@ -0,0 +1,53 @@
+﻿using MetaExchange.API.Models;
+using MetaExchange.Common.Interfaces;
+using MetaExchange.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaExchange.API.Controllers
+{
+    /// <summary>
+    /// Handles trade execution requests.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TradeController : ControllerBase
+    {
+        private readonly IBuyTradeExecutionService _buyExecutionService;
+        private readonly ISellTradeExecutionService _sellExecutionService;
+        private readonly IExchangeDataContextService _seedExecutionData;
+
+        public TradeController(IBuyTradeExecutionService buyExecutionService,
+            ISellTradeExecutionService sellExecutionService,
+            IExchangeDataContextService seedExecutionData)
+        {
+            _buyExecutionService = buyExecutionService;
+            _sellExecutionService = sellExecutionService;
+            _seedExecutionData = seedExecutionData;
+        }
+
+        [HttpPost("execute")]
+        public async Task<IActionResult> ExecuteTrade([FromBody] TradeRequestDto request)
+        {
+            // Here I would just like to point out that I am passing in the exchange data as parameter
+            // due to the assignment scope and requirements, this is in no way a representation
+            // of passing in a database context to a service, which is a bad practice.
+            var exchanges = await _seedExecutionData.GetExchangeDataAsync();
+
+            TradeOrderResult result;
+
+            switch (request.OrderType.ToLower())
+            {
+                case "buy":
+                    result = _buyExecutionService.ExecuteBuyOrder(request.Amount, exchanges);
+                    break;
+                case "sell":
+                    result = _sellExecutionService.ExecuteSellOrder(request.Amount, exchanges);
+                    break;
+                default:
+                    return BadRequest("Order type must be either 'buy' or 'sell'.");
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/MetaExchange.API/Models/TradeRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetaExchange.API.Models
+{
+    // In my API usually I would use fluent validation and pre validate the inputs
+    // such as trimming and putting inputs like this to lower case, but I did not want to
+    // overcomplicate the task, since it is only one endpoint.
+    public class TradeRequestDto
+    {
+        [Required(ErrorMessage = "Order type is required.")]
+        [RegularExpression("^(buy|sell)$", ErrorMessage = "Order type must be either 'buy' or 'sell'.")]
+        public string OrderType { get; set; }
+
+        [Required(ErrorMessage = "BTC amount is required.")]
+        [Range(0.1, double.MaxValue, ErrorMessage = "BTC amount must be greater than 0.")]
+        public decimal Amount { get; set; }
+    }
+}
